cf3dc01bdedbc0f19f7507a492d2f184fc69e6b0
FelixGruener <felix.gruener@essential-ai.com>
2021-11-11 09:20:08 +0800

    Notification added

--- salon-ui/src/components/AppNotificationComponent.js
+++ salon-ui/src/components/AppNotificationComponent.js
@@ -1,43 +1,86 @@
-import React from 'react';
+import {Component, Fragment} from 'react';
 import {Alert} from 'react-bootstrap'
 import { notificationService } from '../services/NotificationService';
+import {appNotification} from "./app-notification";
 
-class AppNotificationComponent extends React.Component {
+const TEN_SECONDS = 10 * 1000
+
+class AppNotificationComponent extends Component {
+
+
     constructor(props) {
         super(props);
-
         this.state = {
-            notifications: []
+            "show": false,
+            "title": '',
+            "variant": '',
+            "message": '',
         };
     }
 
     componentDidMount() {
-        this.subscription = notificationService.getNotifier().subscribe(notification => {
-            if (notification) {
-                this.setState({ notifications: [notification] });
-            } else {
-                this.setState({ notifications: [] });
-            }
+
+        this.subscription = appNotification.onChange().subscribe(res => {
+            this.onNotificationReceived(res);
+        })
+    }
+
+    onNotificationReceived(res) {
+
+
+
+        this.setState({
+            "show": true,
+            "title": res.title,
+            "variant": res.variant,
+            "message": res.message
         });
+        this.resetAfterTenSeconds()
+
     }
 
     componentWillUnmount() {
         this.subscription.unsubscribe();
     }
 
+    reset() {
+
+        this.setState({
+            "show": false,
+            "title": '',
+            "variant": '',
+            "message": '',
+        });
+    }
+
+    resetAfterTenSeconds() {
+        setTimeout(() => {
+            this.reset();
+        }, TEN_SECONDS)
+    }
+
     render() {
-        const { notifications } = this.state;
-        const onClose = () => this.setState({ notifications: [] });
-        return (
-            <div>
-                {notifications.map((notification, index) =>
-                    <Alert  key={index} variant={notification.variant} onClose={onClose} dismissible>
-                        {notification.message}
-                    </Alert >
-                )}
-            </div>
-        );
+
+        const {show, title, message, variant} = this.state
+
+        return <Fragment>
+
+            {show == true && <div className="message-container ">
+
+                <div className="container">
+                    <Alert variant={variant} onClose={() => this.reset()} dismissible>
+                        <Alert.Heading>{title}</Alert.Heading>
+                        <p>
+                            {message}
+                        </p>
+                    </Alert>
+                </div>
+
+            </div>}
+        </Fragment>
+
     }
 }
+
 
 export default AppNotificationComponent;
